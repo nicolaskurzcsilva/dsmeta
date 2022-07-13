708340a20e5c2d0fd232888a8e2432b01562acef
NicolasKurz67 <nicolaskurz67@gmail.com>
2022-07-12 22:32:17 -0400

    Other Components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="Logo DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>Desenvolvido por <a href="https://www.instagram.com/nicolas_kurz_/" target="_blank">@nicolas_kurz_</a></p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
